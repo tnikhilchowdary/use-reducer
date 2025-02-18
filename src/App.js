2d9ee181e6cdae44c9a8ed46c96f3702014bb1a4
tnikhilchowdary <tnikilchowdary@gmail.com>
2025-02-18 00:39:40 -0600

    Initial commit: Counter component using useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useReducer} from "react";
 import './App.css';
 
+const intialState = 0;
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return intialState
+    default:
+      return state;
+  }
+}
+
 function App() {
+      const [count, dispatch] = useReducer(reducer, intialState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Count :{count}</h2>
+      <button onClick={() => dispatch({type: "increment"})}>+</button>
+      <button onClick={() => dispatch({type: "decrement"})}>-</button>
+      <button onClick={() => dispatch({type: "reset"})}>0</button>
+      
+
     </div>
   );
 }
